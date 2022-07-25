a45e33ce9f911a4106916b86af3b4dd038cec660
Bechir Mihoub <bech@Bechirs-iMac.local>
2022-07-24 22:19:23 -0400

    Added disqus to my site

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -59,13 +59,21 @@
             <div class="card-header" style="background-color: #ccc;"><a href="{{route('ringtones.category', [$category->id])}}">{{$category->name}}</a></div>
             @endforeach
         </div>
-        <script src="https://utteranc.es/client.js"
-        repo="typeveron/ringtone-wall"
-        issue-term="pathname"
-        theme="github-light"
-        crossorigin="anonymous"
-        async>
-        </script>
     </div>
+    <div id="disqus_thread"></div>
+<script>
+    var disqus_config = function () {
+    this.page.url = '{{ \Illuminate\Support\Facades\Request::url()}}';  
+    this.page.identifier = '{{$ringtone->download}}'; 
+    };
+
+    (function() { // DON'T EDIT BELOW THIS LINE
+    var d = document, s = d.createElement('script');
+    s.src = 'https://ringtones-and-wallpapers.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+    })();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
 </div>
 @endsection
